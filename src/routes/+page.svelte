e4aa8b944b7feb857fd135dc091ef3c7f9728753
Corsodix <corsodix@gmail.com>
2023-05-23 17:37:30 +0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class="container mx-auto pt-10">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<hr class="!border-t-8" />
+
+	<form class="pt-10">
+		<label class="label pb-5">
+			<span>Input</span>
+			<input class="input" type="text" placeholder="Input" />
+		</label>
+
+		<label class="label pb-5">
+			<span>Select</span>
+			<select class="select">
+				<option value="1">Option 1</option>
+				<option value="2">Option 2</option>
+				<option value="3">Option 3</option>
+				<option value="4">Option 4</option>
+				<option value="5">Option 5</option>
+			</select>
+		</label>
+
+		<label class="label pb-5">
+			<span>Textarea</span>
+			<textarea
+				class="textarea"
+				rows="4"
+				placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+			/>
+		</label>
+	</form>
+</div>
+
+
