f7cb196097b0f6f5acd2b0e1ae9ad0bef1f9b741
Corsodix <corsodix@gmail.com>
2023-05-24 14:21:44 +0400

    sdf

--- src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-// hooks.server.ts
-import { auth } from "$lib/server/lucia";
-import type { Handle } from "@sveltejs/kit";
-
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = auth.handleRequest(event);
-	return await resolve(event);
-};
